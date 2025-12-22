6016778914f5e2697c6ea3205d41245d1f3ab0f3
Jose F Rivas C <josefelixrc7@gmail.com>
2025-12-22 15:47:22 -0400

    feat: Add Verifications controller
    Rel: #65

---
+++ src/controllers/verifications.cpp
@@ -0,0 +1,25 @@
+
+#include "controllers/verifications.h"
+
+using namespace StructBX::Controllers;
+
+Verifications::Verifications(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+    ,user_in_database_(function_data)
+{
+    
+}
+
+Verifications::UserInDatabase::UserInDatabase(Tools::FunctionData& function_data) :
+    Tools::FunctionData(function_data)
+{
+    action = std::make_shared<Functions::Action>("user_in_database_verification_action");
+    action->set_final(false);
+    action->set_sql_code(
+        "SELECT COUNT(*) AS user_count " \
+        "FROM databases_users " \
+        "WHERE id_naf_user = ? AND id_database = (SELECT id FROM `databases` WHERE identifier = ?)"
+    );
+    action->AddParameter_("id_naf_user", "", false);
+    action->AddParameter_("id_database", "", false);
+}
