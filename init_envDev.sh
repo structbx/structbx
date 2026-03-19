ccce8741cfaca1a4110629a9660b27b9a9d24e5c
Jose F Rivas C <josefelixrc7@gmail.com>
2026-03-18 22:45:23 -0400

    chore: Update init_envDev.sh to create necessary directories and copy certificate files

--- init_envDev.sh
+++ init_envDev.sh
@@ -3,10 +3,13 @@
 BASEDIR="$PWD"
 
 mkdir -p "$BASEDIR/build/Debug/DockerVolume" && \
+mkdir -p "$BASEDIR/build/Debug/structbx-web-uploaded" && \
 cd "$BASEDIR/build/Debug" && \
 conan install ../../ --output-folder=./ --build=missing && \
 cd "$BASEDIR" && \
 cp conf/properties.yaml.template build/Debug/properties.yaml && \
 cp conf/properties.yaml.template build/Debug/DockerVolume/properties.yaml && \
 cp conf/cert.pem.template build/Debug/cert.pem && \
+cp conf/cert.pem.template build/Debug/DockerVolume/cert.pem && \
+cp conf/key.pem.template build/Debug/key.pem
 cp conf/key.pem.template build/Debug/DockerVolume/key.pem
