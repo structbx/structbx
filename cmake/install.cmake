d529cc2d48fe63d1f0bd7a4bf8f4328f391d588c
Jose F Rivas C <josefelixrc7@gmail.com>
2026-03-21 13:20:44 -0400

    feat: Enhance build configuration and installation process for generated files
    Rel: #76

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -2,29 +2,6 @@
 # Install files
 include(GNUInstallDirs)
 
-# Create config directory
-set(CONFIG_OUTPUT_DIR "${CMAKE_BINARY_DIR}/config_files")
-file(MAKE_DIRECTORY "${CONFIG_OUTPUT_DIR}")
-
-## a) properties.yaml
-configure_file(
-    "${PROJECT_SOURCE_DIR}/conf/properties.yaml.template"
-    "${CONFIG_OUTPUT_DIR}/properties.yaml"
-    COPYONLY
-)
-## b) cert.pem
-configure_file(
-    "${PROJECT_SOURCE_DIR}/conf/cert.pem.template"
-    "${CONFIG_OUTPUT_DIR}/cert.pem"
-    COPYONLY
-)
-# c) key.pem
-configure_file(
-    "${PROJECT_SOURCE_DIR}/conf/key.pem.template"
-    "${CONFIG_OUTPUT_DIR}/key.pem"
-    COPYONLY
-)
-
 # Install files
 install(
     TARGETS structbx-server
