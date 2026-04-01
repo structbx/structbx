3358f1891b583e74fe523bb7156ad565dda05f15
Jose F Rivas C <josefelixrc7@gmail.com>
2026-03-31 21:32:04 -0400

    feat: Change type of user id to string
    Rel: #97

--- src/security/user.h
+++ src/security/user.h
@@ -19,24 +19,24 @@ class StructBX::Security::User
 {
     public:
         User();
-        User(int id, std::string username, int id_group);
+        User(std::string id, std::string username, std::string id_group);
 
-        int get_id() const { return id_; }
+        std::string get_id() const { return id_; }
         std::string get_username() const { return username_; }
-        int get_id_group() const { return id_group_; }
+        std::string get_id_group() const { return id_group_; }
         std::string get_type() const { return type_; }
         std::string get_status() const { return status_; }
 
-        void set_id(int id) { id_ = id; }
+        void set_id(std::string id) { id_ = id; }
         void set_username(std::string username) { username_ = username; }
-        void set_id_group(int id_group) { id_group_ = id_group; }
+        void set_id_group(std::string id_group) { id_group_ = id_group; }
         void set_type(std::string type) { type_ = type; }
         void set_status(std::string status) { status_ = status; }
 
     private:
-        int id_;
+        std::string id_;
         std::string username_;
-        int id_group_;
+        std::string id_group_;
         std::string type_;
         std::string status_;
 };
