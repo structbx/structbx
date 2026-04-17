f8549b9bf842fd0902f11ec1459204b6316f589a
Jose F Rivas C <josefelixrc7@gmail.com>
2026-04-16 23:26:21 -0400

    doc: Improve HTML documentation and add Latex

--- cmake/doxygen.cmake
+++ cmake/doxygen.cmake
@@ -4,7 +4,7 @@ find_package(Doxygen)
 if(DOXYGEN_FOUND)
 	set(DOXYGEN_IN ${PROJECT_SOURCE_DIR}/docs/Doxyfile.in)
 	set(DOXYGEN_OUT ${PROJECT_BINARY_DIR}/Doxyfile)
-	configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT} @ONLY)
+	configure_file(${DOXYGEN_IN} ${DOXYGEN_OUT})
 
 	message("[StructBX] Doxygen OK")
 else()
