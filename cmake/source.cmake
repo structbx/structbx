c16e25622bf6e7e1e828e2a196cce825d5ff7e50
Jose F Rivas C <josefelixrc7@gmail.com>
2026-05-06 23:52:09 -0400

    chore: Add Sorts class to CMake
    Rel: #96

--- cmake/source.cmake
+++ cmake/source.cmake
@@ -33,6 +33,7 @@ set(
     ${PROJECT_SOURCE_DIR}/src/controllers/tables/permissions.cpp
     ${PROJECT_SOURCE_DIR}/src/controllers/tables/views.cpp
     ${PROJECT_SOURCE_DIR}/src/controllers/tables/filters.cpp
+    ${PROJECT_SOURCE_DIR}/src/controllers/tables/sorts.cpp
     ${PROJECT_SOURCE_DIR}/src/controllers/forms/main.cpp
     ${PROJECT_SOURCE_DIR}/src/core/core.cpp
     ${PROJECT_SOURCE_DIR}/src/core/handler_factory.cpp
