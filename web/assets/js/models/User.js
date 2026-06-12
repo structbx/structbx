dc9fc9b1f2ced6b4c3364f4b27a0e92efb0bc3ab
Jose F Rivas C <josefelixrc7@gmail.com>
2026-06-12 17:27:50 -0400

    feat: Add new functions to User JS Model
    Rel: #102

--- web/assets/js/models/User.js
+++ web/assets/js/models/User.js
@@ -1,4 +1,10 @@
 export class User{
+    async readAll(){
+        return await new wtools.Request("/api/general/users/read").Exec_();
+    }
+    async readByIdentifier(identifier){
+        return await new wtools.Request(`/api/general/users/read/identifier?identifier=${identifier}`).Exec_();
+    }
     async current(){
         return await new wtools.Request("/api/general/users/current/read").Exec_();
     }
