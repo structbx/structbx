6e92bf0f35c4bacc1e11cc50e28aa14b70235cba
Jose F Rivas C <josefelixrc7@gmail.com>
2026-06-22 18:13:41 -0400

    refactor: Update error messages to English in Frontend JS general handling
    Rel: #117

--- web/assets/js/page_form.js
+++ web/assets/js/page_form.js
@@ -1,6 +1,13 @@
 import { FormsController } from './modules/forms_controller.js';
+import { I18n } from './i18n/i18n.js';
 
 $(document).ready(() =>
 {
+    if (!window.structbxI18n)
+    {
+        window.structbxI18n = new I18n();
+    }
+    window.structbxI18n.translateDOM();
+
     new FormsController().init();
 });
--- web/assets/js/structbx.js
+++ web/assets/js/structbx.js
@@ -2,6 +2,7 @@ import { LoginController } from './modules/login_controller.js';
 import { StartController } from './modules/start_controller.js';
 import { TableController } from './modules/table_controller.js';
 import { SettingsController } from './modules/settings_controller.js';
+import { I18n } from './i18n/i18n.js';
 
 const Pages = {
     'Login': LoginController
@@ -11,6 +12,12 @@ const Pages = {
 };
 
 $(document).ready(() => {
+    if (!window.structbxI18n)
+    {
+        window.structbxI18n = new I18n();
+    }
+    window.structbxI18n.translateDOM();
+
     const pageName = $('body').data('page');
     const ControllerClass = Pages[pageName];
 
